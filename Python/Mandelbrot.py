26ee409440f406cbf2f18af62013788562ebcf3c
Gabnaim <gabnai1@yahoo.com>
2023-11-08 18:25:12 -0600

    Mandelbrot.py: working with one color

--- Python/Mandelbrot.py
+++ Python/Mandelbrot.py
@@ -1,48 +1,44 @@
-import numpy as np
+import math
 from PIL import Image
 
 ITERATIONS = 46
-SIZE = 176
+SIZE = 1000
 DIAMETER = 2.5
 
 step = DIAMETER / SIZE
 yRange = SIZE // 2
 xStart = -0.5 - DIAMETER / 2
 yStart = 0
+colorStep = 255 / ITERATIONS
 
-colors = ("#0000AA", "#88DDFF", "#FF8800", "#000000")
-ncolors = len(colors)
-
-def mandel(real,imag):
-    z_r = 0
-    z_i = 0
-    z_r_squared = 0
-    z_i_squared = 0
+def mandel(x,y):
+    real = 0
+    imag = 0
     for i in range(ITERATIONS):
-        z_r_squared = z_r * z_r
-        z_i_squared = z_i * z_i
-        z_r = z_r_squared - z_i_squared + real
-        z_i = 2 * z_r * z_i + imag
+        realSquared = real * real
+        imagSquared = imag * imag
+        tempReal = realSquared - imagSquared + x
+        imag = 2 * real * imag + y
+        real = tempReal
 
-        if z_r_squared + z_r_squared > 4:
+        if realSquared + realSquared >= 4:
             return i     
     return ITERATIONS
     
 img = Image.new( 'RGB', (SIZE, SIZE), "black") # create a new black image
 pixels = img.load() # create the pixel map
 
-real = xStart
-imag = yStart
+x = xStart
+y = yStart
 
 for x in range(SIZE):    # for every col:
     for y in range(yRange):    # For every row
-        i = mandel(real, imag)
-        color = colors[i//2 % ncolors] 
-        
-        pixels[x,y] = (x,y, color) # set the colour accordingly
-        pixels[x, SIZE - y] = (x, SIZE - y, color)
-        imag += step
-    imag = yStart
-    real += step
+        i = mandel(x, y)
+        color = math.floor(i * colorStep)
+        pixels[x,y + yRange ] = (color, 0, 0)
+        pixels[x,yRange - y] = (color, 0, 0)
+        y += step
+    y = yStart
+    x += step
 
 img.show()
