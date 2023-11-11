a74c5e7b49abd69279aacac05949c716b71b174e
Gabnaim <gabnai1@yahoo.com>
2023-11-11 14:06:50 -0600

    Block segmentation.py now deals with margins

--- Python/Block segmentation.py
+++ Python/Block segmentation.py
@@ -4,22 +4,24 @@ from scene import *
 import numpy as np
 from threading import Thread
 
-IMAGESIZE = 176
+IMAGESIZE = 177
 SCALE = 4
 BLOCKSIZE = 16
-MARGIN = 30
+DISPLAYMARGIN = 30
 displaySize = IMAGESIZE * SCALE
 pixelSize = SCALE
 rows = cols = IMAGESIZE // BLOCKSIZE
-rectSize = 14
-blockCenter = math.floor((BLOCKSIZE - rectSize) / 2)
+rowMargin = colMargin = (IMAGESIZE - rows * BLOCKSIZE ) // 2
 
+rectSize = math.floor(BLOCKSIZE * 0.75)
+blockCenter = math.floor((BLOCKSIZE - rectSize) / 2)
+imgCenter = IMAGESIZE // 2
 
 slowDown = 0.05
-pixels = np.full((IMAGESIZE, IMAGESIZE, 4), (0, 0, 1, 1))
+pixels = np.full((IMAGESIZE, IMAGESIZE, 4),(0, 0, 1, 1))
 
 def generateImage():
-	imgX = imgY = blockCenter
+	imgX = imgY = startX = startY = blockCenter + rowMargin
 	for col in range(cols):
 		for row in range(rows):
 			time.sleep(slowDown)
@@ -28,19 +30,19 @@ def generateImage():
 					pixels[imgX + i, imgY + j] = (1, 0, 0, 1)
 			imgY += BLOCKSIZE
 		imgX += BLOCKSIZE
-		imgY = blockCenter
+		imgY = startY
 			
 			
 class ImageRender(Scene):
 	def draw(self):
 		background('white')
 		fill('blue')
-		rect(MARGIN, MARGIN, displaySize, displaySize)
+		rect(DISPLAYMARGIN, DISPLAYMARGIN, displaySize, displaySize)
 		for px in range(IMAGESIZE):
 			for py in range(IMAGESIZE):
 				color = pixels[px, py]
 				fill(tuple(color))
-				rect(px * SCALE + MARGIN, py * SCALE + MARGIN, pixelSize, pixelSize)
+				rect(px * SCALE + DISPLAYMARGIN, py * SCALE + DISPLAYMARGIN, pixelSize, pixelSize)
 				
 def showImage():
 	run(ImageRender())
