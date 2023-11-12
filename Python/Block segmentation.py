4a69042824498fb4b1e912c9d14a7fd9009ab04d
Gabnaim <gabnai1@yahoo.com>
2023-11-12 17:26:35 -0600

    Block segmentation.py generates squares in a spiral order with random colors

--- Python/Block segmentation.py
+++ Python/Block segmentation.py
@@ -10,36 +10,47 @@ BLOCKSIZE = 16
 DISPLAYMARGIN = 30
 displaySize = IMAGESIZE * SCALE
 pixelSize = SCALE
-rows = cols = IMAGESIZE // BLOCKSIZE
+size = IMAGESIZE // BLOCKSIZE
 
-rectSize = math.floor(BLOCKSIZE * 0.75)
+rectSize = math.floor(BLOCKSIZE * 1)
 blockCenter = math.floor((BLOCKSIZE - rectSize) / 2)
-imgCenter = IMAGESIZE // 2
 
 slowDown = 0.05
-pixels = np.full((IMAGESIZE, IMAGESIZE, 4),(0, 0, 1, 1))
-
-mid = rows // 2
+pixels = np.full((IMAGESIZE, IMAGESIZE, 4),(0.0, 0.0, 1.0, 1.0))
 
 def generateBlock(imgX, imgY):	
-	imgX -= blockCenter
-	imgY -= blockCenter
 	time.sleep(slowDown)
+	r = np.random.random()
+	g = np.random.random()
+	b = np.random.random()
 	for i in range(rectSize):
 		for j in range(rectSize):
-			pixels[imgX + i, imgY + j] = (1, 0, 0, 1)
+			pixels[imgX + i, imgY + j] = (r, g, b, 1)
 	
+def spiral(size, process):
+	midX = size // 2
+	midY = math.ceil(size / 2 - 1)
+	x = midX
+	y = midY
+	yDir = 0
+	xDir = 1 if (size % 2 == 1) else -1
+	while 0 <= x < size and 0 <= y < size:
+		generateBlock(x * BLOCKSIZE, y * BLOCKSIZE)
+		x += xDir
+		y += yDir
+		# direction changes
+		if x == y and y <= midY: # up
+			xDir, yDir = 0, 1
+		elif x == y + 1 and y >= midY: #down
+			xDir, yDir = 0, -1
+		elif x + y == size - 1:
+			if y >= midY: #right
+				xDir, yDir = 1, 0
+			else: # left
+				xDir, yDir = -1, 0
+				
 def generateImage():
-	generateBlock(imgCenter, imgCenter)
-	for col in range(0, mid, 1):
-		colOffset = col * BLOCKSIZE
-		for row in range(0, mid, 1):
-			rowOffset = row * BLOCKSIZE
-			generateBlock(imgCenter + rowOffset, imgCenter + colOffset)
-			generateBlock(imgCenter - rowOffset, imgCenter + colOffset)
-			generateBlock(imgCenter + rowOffset, imgCenter - colOffset)
-			generateBlock(imgCenter - rowOffset, imgCenter - colOffset)
-			
+	spiral(size, generateBlock)
 			
 class ImageRender(Scene):
 	def draw(self):
