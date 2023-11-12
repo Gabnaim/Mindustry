1ee5881794c44e66ff3bd6e10f8b07cc11288b33
Gabnaim <gabnai1@yahoo.com>
2023-11-12 16:38:51 -0600

    Separated generate block

--- Python/Block segmentation.py
+++ Python/Block segmentation.py
@@ -4,14 +4,13 @@ from scene import *
 import numpy as np
 from threading import Thread
 
-IMAGESIZE = 177
+IMAGESIZE = 176
 SCALE = 4
 BLOCKSIZE = 16
 DISPLAYMARGIN = 30
 displaySize = IMAGESIZE * SCALE
 pixelSize = SCALE
 rows = cols = IMAGESIZE // BLOCKSIZE
-rowMargin = colMargin = (IMAGESIZE - rows * BLOCKSIZE ) // 2
 
 rectSize = math.floor(BLOCKSIZE * 0.75)
 blockCenter = math.floor((BLOCKSIZE - rectSize) / 2)
@@ -20,17 +19,26 @@ imgCenter = IMAGESIZE // 2
 slowDown = 0.05
 pixels = np.full((IMAGESIZE, IMAGESIZE, 4),(0, 0, 1, 1))
 
+mid = rows // 2
+
+def generateBlock(imgX, imgY):	
+	imgX -= blockCenter
+	imgY -= blockCenter
+	time.sleep(slowDown)
+	for i in range(rectSize):
+		for j in range(rectSize):
+			pixels[imgX + i, imgY + j] = (1, 0, 0, 1)
+	
 def generateImage():
-	imgX = imgY = startX = startY = blockCenter + rowMargin
-	for col in range(cols):
-		for row in range(rows):
-			time.sleep(slowDown)
-			for i in range(rectSize):
-				for j in range(rectSize):
-					pixels[imgX + i, imgY + j] = (1, 0, 0, 1)
-			imgY += BLOCKSIZE
-		imgX += BLOCKSIZE
-		imgY = startY
+	generateBlock(imgCenter, imgCenter)
+	for col in range(0, mid, 1):
+		colOffset = col * BLOCKSIZE
+		for row in range(0, mid, 1):
+			rowOffset = row * BLOCKSIZE
+			generateBlock(imgCenter + rowOffset, imgCenter + colOffset)
+			generateBlock(imgCenter - rowOffset, imgCenter + colOffset)
+			generateBlock(imgCenter + rowOffset, imgCenter - colOffset)
+			generateBlock(imgCenter - rowOffset, imgCenter - colOffset)
 			
 			
 class ImageRender(Scene):
