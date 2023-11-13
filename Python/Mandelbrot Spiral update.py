52ad5e1e7f43af216fdf662041553ba7567f971e
Gabnaim <gabnai1@yahoo.com>
2023-11-12 21:18:42 -0600

    Mandelbrot Spiral update.py: created Mandel class

--- Python/Mandelbrot Spiral update.py
+++ Python/Mandelbrot Spiral update.py
@@ -5,50 +5,52 @@ import numpy as np
 from threading import Thread
 
 IMAGESIZE = 176
-SCALE = 4
 BLOCKSIZE = 16
-DISPLAYMARGIN = 30
-displaySize = IMAGESIZE * SCALE
-pixelSize = SCALE
-size = IMAGESIZE // BLOCKSIZE
 
-rectSize = math.floor(BLOCKSIZE * 1)
-blockCenter = math.floor((BLOCKSIZE - rectSize) / 2)
+MAXITER = 200
+VIEWSIZE = 2
+OFFSET = (-0.5, 0)
 
 pixels = np.full((IMAGESIZE, IMAGESIZE, 4),(0.0, 0.0, 1.0, 1.0))
 
-MAXITER = 20
-VIEWSIZE = 2
-OFFSET = Point(-0.5, 0)
-mbStep = VIEWSIZE / IMAGESIZE
-colorStep = 1 / MAXITER
-slowDown = 1e-4 / MAXITER
+class Mandel:
+	def __init__(self, viewSize, offset, maxIter, imageSize):
+		self.viewSize = viewSize
+		self.offsetX = offset[0] - viewSize / 2
+		self.offsetY = offset[1] - viewSize / 2
+		self.maxIter = maxIter
+		self.imageSize = imageSize
+		self.step = viewSize / imageSize
+		self.colorStep = 1 / maxIter
+		self.slowDown = 1e-4 / maxIter
 
-def mandel(x0,y0):
-	zR = 0
-	zI = 0
-	for i in range(MAXITER):
-		zRSquared = zR * zR
-		zISquared = zI * zI
-		zI = 2 * zR * zI + y0
-		zR = zRSquared - zISquared + x0
-		
-		if zRSquared + zISquared > 4:
-			return i
-	return MAXITER
+	def mandel(self, x0, y0):
+		zR = 0
+		zI = 0
+		for i in range(self.maxIter):
+			zRSquared = zR * zR
+			zISquared = zI * zI
+			zI = 2 * zR * zI + y0
+			zR = zRSquared - zISquared + x0
+			
+			if zRSquared + zISquared > 4:
+				return i
+		return self.maxIter
 	
-def generateBlock(imgX, imgY):
-	blockSpiral = Spiral(BLOCKSIZE)
-	real = OFFSET.x + imgX * mbStep - VIEWSIZE / 2
-	imag = OFFSET.y	+ imgY * mbStep - VIEWSIZE / 2
-	while not blockSpiral.atEnd():
-		time.sleep(slowDown)
-		i, j = blockSpiral.next()
-		mb = mandel(real + i * mbStep, imag + j * mbStep)
-		if mb == MAXITER:
-			pixels[imgX + i, imgY + j] = (0, 0, 0, 1)
-		else:
-			pixels[imgX + i, imgY + j] = (0, mb * colorStep, 1, 1) 
+	def generateBlock(self, imgX, imgY, blockSize):
+		imgX *= blockSize
+		imgY *= blockSize
+		blockSpiral = Spiral(blockSize)
+		real = self.offsetX + imgX * self.step
+		imag = self.offsetY + imgY * self.step
+		while not blockSpiral.atEnd():
+			time.sleep(self.slowDown)
+			i, j = blockSpiral.next()
+			mb = self.mandel(real + i * self.step, imag + j * self.step)
+			if mb == self.maxIter:
+				pixels[imgX + i, imgY + j] = (0, 0, 0, 1)
+			else:
+				pixels[imgX + i, imgY + j] = (0, mb * self.colorStep, 1, 1) 
 		
 class Spiral:
 	def __init__(self, size):
@@ -83,28 +85,39 @@ class Spiral:
 		self.y += self.yDir
 		self.getDirection()
 		return value
-		
-spiral = Spiral(size)
-def generateImage():
+
+def generateFractal():
+	numBlocks = IMAGESIZE // BLOCKSIZE
+	spiral = Spiral(numBlocks)
+	mandel = Mandel(viewSize = VIEWSIZE,
+									offset = OFFSET,
+									maxIter = MAXITER,
+									imageSize = IMAGESIZE)
 	while not spiral.atEnd():
 		x, y = spiral.next()
-		generateBlock(x * BLOCKSIZE, y * BLOCKSIZE)
+		mandel.generateBlock(x, y, BLOCKSIZE)
 		
 class ImageRender(Scene):
+	def setup(self):
+		self.imageSize = IMAGESIZE
+		self.scale = 4
+		self.margin = 30
+		self.displaySize = self.imageSize * self.scale
+		
 	def draw(self):
 		background('white')
 		fill('blue')
-		rect(DISPLAYMARGIN, DISPLAYMARGIN, displaySize, displaySize)
-		for px in range(IMAGESIZE):
-			for py in range(IMAGESIZE):
+		rect(self.margin, self.margin, self.displaySize, self.displaySize)
+		for px in range(self.imageSize):
+			for py in range(self.imageSize):
 				color = pixels[px, py]
 				fill(tuple(color))
-				rect(px * SCALE + DISPLAYMARGIN, py * SCALE + DISPLAYMARGIN, pixelSize, pixelSize)
+				rect(px * self.scale + self.margin, py * self.scale + self.margin, self.scale, self.scale)
 				
 def showImage():
 	run(ImageRender())
 	
-t1 = Thread(target=generateImage)
+t1 = Thread(target=generateFractal)
 t2 = Thread(target=showImage)
 
 t1.start()
